b941257a42beb642f5c1883b7eab6e2a735d52ac
leandrozel <leandro.ferreira131@etec.sp.gov.br>
2025-08-13 20:00:04 -0300

    Algumas melhorias quaisquer

--- obj/Debug/net9.0/aula02RH.AssemblyInfo.cs
+++ obj/Debug/net9.0/aula02RH.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aula02RH")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddf907834c7a62eb1353396bed63ed0e5e0531c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("aula02RH")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aula02RH")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
